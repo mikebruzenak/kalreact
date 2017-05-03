1f970505fe3845d296ec73accc9670c51e69d8b5
mikebruzenak <mike.bruzenak@gmail.com>
2017-05-03 05:16:12 -0500

    rails validations

--- client/app/bundles/Appointments/components/appointments.jsx
+++ client/app/bundles/Appointments/components/appointments.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import AppointmentForm from './appointment_form'
 import { AppointmentsList } from './appointments_list'
 import update from 'immutability-helper'
+import { FormErrors } from './FormErrors'
 
 export default class Appointments extends React.Component{
     constructor(props, _railsContext) {
         super(props)
         this.state = {
             appointments: this.props.appointments,
-            title: 'Team standup meeting',
-            appt_time: '25 January 2016 9am'
+            title: '',
+            appt_time: '',
+            formErrors: {}
         }
     }
 
@@ -20,7 +22,18 @@ export default class Appointments extends React.Component{
     handleFormSubmit() {
         const appointment = {title: this.state.title, appt_time: this.state.appt_time};
         $.post('/appointments', {appointment: appointment})
-            .done((data) => { this.addNewAppointment(data) })
+            .done((data) => {
+                this.addNewAppointment(data)
+                this.resetFormErrors()
+            })
+            .fail((response) => {
+                console.log(response)
+                this.setState({formErrors: response.responseJSON})
+            })
+    }
+
+    resetFormErrors () {
+        this.setState({formErrors: {}})
     }
 
     addNewAppointment(appointment) {
@@ -35,6 +48,7 @@ export default class Appointments extends React.Component{
     render() {
         return (
             <div>
+                <FormErrors formErrors = {this.state.formErrors} />
                 <AppointmentForm input_title={this.state.title}
                                  input_appt_time={this.state.appt_time}
                                  onUserInput={(obj) => this.handleUserInput(obj)}
