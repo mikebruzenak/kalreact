f457a89073848e2cafe7029d399df57cd3ef4cf8
mikebruzenak <mike.bruzenak@gmail.com>
2017-05-06 07:10:18 -0500

    added Appointment show in controller and ajax call in Appointment

--- client/app/bundles/Appointments/components/Appointment.jsx
+++ client/app/bundles/Appointments/components/Appointment.jsx
@@ -1,10 +1,13 @@
-import React, { PropTypes } from 'react'
-import { Link } from 'react-router-dom'
-import { formatDate } from '../utils/format'
+import React, {PropTypes} from 'react'
+import {Link} from 'react-router-dom'
+import {formatDate} from '../utils/format'
 
 export default class Appointment extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            appointment: props.appointment
+        }
     }
 
     static propTypes = {
@@ -15,14 +18,26 @@ export default class Appointment extends React.Component {
         appointment: {}
     }
 
+    componentDidMount() {
+        if (this.props.match) {
+            $.ajax({
+                type: 'GET',
+                url: `/appointments/${this.props.match.params.id}`,
+                dataType: 'JSON'
+            }).done((data) => {
+                this.setState({appointment: data})
+            })
+        }
+    }
+
     render() {
-        return(
+        return (
             <div className="appointment">
                 <h2>Appointment</h2>
-                <Link to={`/appointments/${this.props.appointment.id}`}>
-                    <h3>{this.props.appointment.title}</h3>
+                <Link to={`/appointments/${this.state.appointment.id}`}>
+                    <h3>{this.state.appointment.title}</h3>
                 </Link>
-                <p>{formatDate(this.props.appointment.appt_time)}</p>
+                <p>{formatDate(this.state.appointment.appt_time)}</p>
             </div>
         )
     }
